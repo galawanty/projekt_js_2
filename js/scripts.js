5ff362deb0b0f2abc86d22f2a807fd8fe03262aa
Łukasz Wasiluk <galawanty@gmail.com>
2017-02-27 21:22:11 +0100

    alert komentarz = 2 komentarze

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// plik scripts.js
+//komentarz liniowy...............................................
+/* 
+   komentarz blokowy........................................
+   komentarz blokowy........................................
+   komentarz blokowy........................................
+*/   
+
+var name = prompt('Wpisz swoje imię');
+alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+
+/*
+console.log(c);
+*/
